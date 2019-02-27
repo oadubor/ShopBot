075558c61628e67971e2eef641ba3eb4f5c5185f
Hans Kumar <hansk@andrew.cmu.edu>
2019-02-27 15:24:54 -0500

    added ros workspace

---
+++ shopbot_ws/build/aruco_find/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hansk/shopbot_ws/src/aruco_find
+# Build directory: /home/hansk/shopbot_ws/build/aruco_find
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
