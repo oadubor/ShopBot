8653b7f6446cc59780d915c03301464dea8a8347
hans <hansk@andrew.cmu.edu>
2019-04-07 22:48:01 +0000

    no more build files

--- shopbot_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hansk/shopbot_ws/src
-# Build directory: /home/hansk/shopbot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(aruco_find)
--- shopbot_ws/build/aruco_find/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hansk/shopbot_ws/src/aruco_find
-# Build directory: /home/hansk/shopbot_ws/build/aruco_find
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
