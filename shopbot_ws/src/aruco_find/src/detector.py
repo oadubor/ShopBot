7f93b225132f907ef7588ea6f991fae39aa39227
Hans Kumar <hansk@andrew.cmu.edu>
2019-04-30 15:53:29 -0400

    added cv stuff

--- shopbot_ws/src/aruco_find/src/detector.py
+++ shopbot_ws/src/aruco_find/src/detector.py
@@ -2,6 +2,7 @@ import cv2
 import cv2.aruco as aruco
 import numpy as np
 import glob
+from rot import rotationMatrixToEulerAngles
 
  
 
@@ -21,11 +22,19 @@ def detector(image):
     corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 
     font = cv2.FONT_HERSHEY_SIMPLEX #font for displaying text (below)
+    yAngles = -1.0
+    xDist = -1.0 
 
     if np.all(ids != None):
         rvec, tvec,_ = aruco.estimatePoseSingleMarkers(corners[0], 0.1, mtx, dist) #Estimate pose of each marker and return the values rvet and tvec---different from camera coefficients
         #(rvec-tvec).any() # get rid of that nasty numpy value array error
-        print(tvec)
+        
+        roationMatrix = cv2.Rodrigues(rvec[0])[0]
+        eulerAngles = rotationMatrixToEulerAngles(roationMatrix)
+
+        yAngles = eulerAngles[1]
+        xDist = tvec[0][0][0]
+        
 
         aruco.drawAxis(image, mtx, dist, rvec[0], tvec[0], 0.1) #Draw Axis
         aruco.drawDetectedMarkers(image, corners) #Draw A square around the markers
@@ -34,6 +43,6 @@ def detector(image):
         ###### DRAW ID #####
         cv2.putText(image, "Id: " + str(ids), (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
 
-    return image
+    return image, xDist, yAngles
 
 
--- shopbot_ws/src/aruco_find/src/image_sub.py
+++ shopbot_ws/src/aruco_find/src/image_sub.py
@@ -5,7 +5,8 @@ import roslib
 import sys
 import rospy
 import cv2
-from std_msgs.msg import String
+from std_msgs.msg import String 
+from std_msgs.msg import Float64 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from detector import detector
@@ -17,6 +18,9 @@ class image_converter:
   def __init__(self):
     self.image_pub = rospy.Publisher("image_topic_2",Image)
 
+    self.boxAngle_pub = rospy.Publisher("angleBox",Float64)
+    self.boxPos_pub = rospy.Publisher("posBox",Float64)
+
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
 
@@ -27,12 +31,16 @@ class image_converter:
       print(e)
 
 
-    image_aruco = detector(cv_image)
+    image_aruco,pos,angle = detector(cv_image)
+
     cv2.imshow("Image window", image_aruco)
     cv2.waitKey(3)
 
     try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(image_aruco, "bgr8"))
+      self.boxAngle_pub.publish(angle)
+      self.boxPos_pub.publish(pos)
+      
     except CvBridgeError as e:
       print(e)
 
